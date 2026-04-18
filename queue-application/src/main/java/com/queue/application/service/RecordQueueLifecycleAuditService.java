c1d330fc2571e1b65ab4efd9bf1fdcf278548a54
jd <jd0922@naver.com>
2026-04-18 21:46:37 +0900

    라이프사이클 audit 적재 유스케이스와 포트 경계 추가

---
+++ queue-application/src/main/java/com/queue/application/service/RecordQueueLifecycleAuditService.java
@@ -0,0 +1,21 @@
+package com.queue.application.service;
+
+import com.queue.application.dto.RecordQueueLifecycleAuditCommand;
+import com.queue.application.port.in.RecordQueueLifecycleAuditUseCase;
+import com.queue.application.port.out.QueueLifecycleAuditCommandPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class RecordQueueLifecycleAuditService implements RecordQueueLifecycleAuditUseCase {
+
+    private final QueueLifecycleAuditCommandPort queueLifecycleAuditCommandPort;
+
+    @Override
+    public boolean record(RecordQueueLifecycleAuditCommand command) {
+        return queueLifecycleAuditCommandPort.insertIfAbsent(command);
+    }
+}
